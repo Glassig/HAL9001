119d975cccff68590de150dff40ff57f2b3add52
Angelina <angelina.v.gegerfelt@gmail.com>
2017-09-23 15:43:21 +0200

    Made divvyupthebooty readable and inserted template-strings

--- DivvyUpTheBooty.js
+++ DivvyUpTheBooty.js
@@ -1,30 +1,31 @@
-function ParseMoney(rawString) {
-  var groups = /^(@?)([-\d]+)\s*([\s\wåäö]+)$/.exec(rawString)
-  if (groups === null || groups.length !== 4)
-    throw new Error("Could not parse string " + rawString + ", result became " + groups)
+function DivvyUpTheBooty(angelinaList, andreasList) {
+  let debt = 0
+  debt -= handleList(angelinaList)
+  debt += handleList(andreasList)
   return {
-    amount: parseInt(groups[2]),
-    owedDontSplit: groups[1] === "@"
+    whoOwe: debt === 0 ? "Even" : (debt > 0 ? "Angelina" : "Andreas"),
+    amount: Math.abs(debt)
   }
 }
 
-function DivvyUpTheBooty(angelinaList, andreasList) {
-  let angelinaOwe = 0
-  function handleList(list) {
-    let total = 0
-    list.forEach((item) => {
-      let money = ParseMoney(item)
-      if (money.owedDontSplit)
-        total += money.amount
-      else
-        total += money.amount / 2
-    })
-    return total
-  }
-  angelinaOwe -= handleList(angelinaList)
-  angelinaOwe += handleList(andreasList)
+function handleList(list) {
+  let total = 0
+  list.forEach((item) => {
+    let money = ParseMoney(item)
+    if (money.owedDontSplit)
+      total += money.amount
+    else
+      total += money.amount / 2
+  })
+  return total
+}
+
+function ParseMoney(rawString) {
+  var groups = /^(@?)([-\d]+)\s*([\s\wåäö]+)$/.exec(rawString)
+  if (groups === null || groups.length !== 4)
+    throw new Error(`Could not parse string ${rawString} result became ${groups}`)
   return {
-    whoOwe: angelinaOwe === 0 ? "Even" : (angelinaOwe > 0 ? "Angelina" : "Andreas"),
-    amount: Math.abs(angelinaOwe)
+    amount: parseInt(groups[2]),
+    owedDontSplit: groups[1] === "@"
   }
 }
